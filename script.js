8f5cabc6b352fa0b1cfea95286aef2e1e973f288
Yusril malik <shuraputrapurnama@gmail.com>
2025-08-30 19:07:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+
+    menuBtn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    });
+     // Masukkan nomor WhatsApp kamu
+  const phoneNumber = "+6282248536718"; // ganti dengan nomor kamu
+  document.getElementById("waLink").href = "https://web.whatsapp.com/";{+6282248536718}
+  
